0f7ceb3f4e6451c7db692918833d028033d1085a
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-18 20:30:41 +0530

    deleted All and created new one with error

---
+++ src/MainContent.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function MainContent() {
+    return (
+            <main>
+            <h1>Hello World!<h1/>
+                <p>This is where most of my content will go...</p>
+            </main>
+)
+}
+
+export default MainContent
