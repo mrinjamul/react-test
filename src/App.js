0cf729aa093ca21fca80712afe9e17e56d6675a0
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-25 02:54:06 +0530

    switch to class-based Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react"
+import React, {Component} from "react"
 import List from "./components/List"
 import Data from "./components/listData"
 
-function App() {
-  const data = Data.map(data => <List key={data.id} name={data.name} tel={data.tel} />)
-    return (
-        <div>
-          {data}
-        </div>
+// function App() {
+//   const data = Data.map(data => <List key={data.id} name={data.name} tel={data.tel} />)
+//     return (
+//         <div>
+//           {data}
+//         </div>
+//     )
+// }
+class App extends Component {
+  render() {
+    const data = Data.map(data => <List key={data.id} name={data.name} tel={data.tel} />)
+    return(
+      <div>
+        {data}
+      </div>
     )
+  }
 }
 
 export default App
