9df84c5369114e52513144d06d0a9c4cb8e32507
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-25 02:57:47 +0530

    class-based Components List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {Component} from "react";
 
-function List(props) {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.tel}</h1>
-    </div>
-  );
+// function List(props) {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h1>{props.tel}</h1>
+//     </div>
+//   );
+// }
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{props.name}</h1>
+        <h1>{props.tel}</h1>
+      </div>
+    )
+  }
 }
 
 export default List;
