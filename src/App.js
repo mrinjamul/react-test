34e4d4807ed3c86b81463a05ba1c88f7e913dd77
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-04-20 23:37:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
-import "./App.css"
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            count: 0
-        }
-        this.handleClick = this.handleClick.bind(this)
-        this.handleReset = this.handleReset.bind(this)
-    }
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
 
-    handleClick() {
-        this.setState(prevState => {
-            return {
-                count: prevState.count + 1
-            }
-        })
-    }
-    handleReset() {
-      this.setState({ count: 0 })
-    }
+  handleClick() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+  handleReset() {
+    this.setState({ count: 0 });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.handleClick}>Change!</button><br/>
-                <button onClick={this.handleReset}>Reset</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+        <br />
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
