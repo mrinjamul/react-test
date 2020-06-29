8ec4582e461abe5ec908064ab62fb845ed204525
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-06-30 00:03:53 +0530

    Add progress

--- src/App.js
+++ src/App.js
@@ -1,64 +1,19 @@
-import React, { Component } from "react";
-import axios from "axios";
-import User from "./components/User";
-
+import React from "react";
 import "./App.css";
-import "./styles/Main.sass";
+
 import ProgressBar from "./components/ProgressBar";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      percent: 0.4,
-      count: 0,
-      name: "",
-      persons: [],
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
-
+class App extends React.Component {
+  state = {
+    percent: 0.6,
+  };
   updateProgress = (field, val) => {
     this.setState({ [field]: val });
   };
 
-  handleClick() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1,
-      };
-    });
-  }
-  handleReset() {
-    this.setState({ count: 0 });
-  }
-  componentDidMount() {
-    axios.get(`http://localhost:8080/users`).then((res) => {
-      const persons = res.data;
-      this.setState({ persons });
-      console.log(persons);
-    });
-  }
-  handleChange = (event) => {
-    this.setState({ name: event.target.value });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    const user = {
-      name: this.state.name,
-    };
-
-    axios.post(`http://localhost:8080/users`, { user }).then((res) => {
-      console.log(res);
-      console.log(res.data);
-    });
-  };
-
   render() {
     return (
-      <div>
+      <div className="App">
         <div className="div">
           <ProgressBar width={400} percent={this.state.percent} />
           <button
@@ -69,33 +24,8 @@ class App extends Component {
             Add 10%
           </button>
         </div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Change!</button>
-        <br />
-        <button onClick={this.handleReset}>Reset</button>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name:
-            <input type="text" name="name" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-        <div>
-          <ul>
-            {this.state.persons.map((person) => (
-              <User
-                key={person.id}
-                id={person.id}
-                name={person.name}
-                username={person.username}
-                email={person.email}
-              />
-            ))}
-          </ul>
-        </div>
       </div>
     );
   }
 }
-
 export default App;
