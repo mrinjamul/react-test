8e5bb5143aad89296092612206c757f78ca9e3f1
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-07-12 19:51:34 +0530

    fix: Simplified

--- src/components/ChangeMe.js
+++ src/components/ChangeMe.js
@@ -4,19 +4,16 @@ import "./ChangeMe.css";
 
 function ChangeMe() {
   let [state, SetState] = useState("red");
-  const handleClick = () => {
-    // if (state === "red") {
-    //   SetState("blue");
-    // }
-    // if (state === "blue") {
-    //   SetState("red");
-    // }
-    state === "red" ? SetState("blue") : SetState("red");
-  };
   return (
     <div>
       <h1 className={`font ${state}`}>Change Me</h1>
-      <button onClick={handleClick}>ChangeMe</button>
+      <button
+        onClick={() => {
+          state === "red" ? SetState("blue") : SetState("red");
+        }}
+      >
+        ChangeMe
+      </button>
     </div>
   );
 }
